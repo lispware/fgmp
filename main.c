014edd33fa6b676daf742ff30ff8192d0b6971ce
CK Kashyap <ckkashyap@gmail.com>
2022-08-07 21:50:12 -0700

    step

--- main.c
+++ main.c
@@ -1,41 +1,43 @@
 #include "gmp.h"
 
-void factorial(unsigned int n, mpz_t result) {
-  mpz_set_ui(result, 1);
-  while (n > 1) {
-      mpz_mul_ui(result, result, n);
-      n = n-1;
-  }
-}
-
-#if 0 
-int main() {
-  unsigned int n = 34530; // promt user
-  mpz_t fact;
-  mpz_init(fact);
-  factorial(n, fact);
-  mpz_out_str(stdout, 10, fact);
-  mpz_clear(fact);
-  return 0;
-}
-#endif
-
-
-MP_INT fact(MP_INT n)
+void factorial(unsigned int n, mpz_t result)
 {
-    
+    mpz_set_ui(result, 1);
+    while (n > 1)
+    {
+        mpz_mul_ui(result, result, n);
+        n = n-1;
+    }
 }
 
 int main()
 {
-  //unsigned int n = 34530; // promt user
-  unsigned int n = 100; // promt user
-  mpz_t fact;
-  mpz_init(fact);
-  factorial(n, fact);
-  char *buffer = mpz_get_str(NULL, 10, fact);
-  printf("%s\n", buffer);
-  mpz_clear(fact);
+    //unsigned int n = 34530; // promt user
+    unsigned int n = 5; // promt user
+    mpz_t fact;
+    mpz_init(fact);
+    factorial(n, fact);
+    char *buffer = mpz_get_str(NULL, 10, fact);
+    printf("%s\n", buffer);
+
+
+    long long out = mpz_get_ui(fact);
+    printf("THE NUMBER = %d\n", out);
+    mpz_clear(fact);
+
+
+
+    mpz_t BASE, RES;
+    mpz_init(BASE);
+    mpz_init(RES);
+    mpz_set_ui(BASE, 2);
+    mpz_pow_ui(RES, BASE, 31);
+
+    buffer = mpz_get_str(NULL, 10, RES);
+    printf("RES = %s\n", buffer);
+    out = mpz_get_ui(RES);
+    printf("THE NUMBER = %d\n", out);
+
 
 
     return 0;
